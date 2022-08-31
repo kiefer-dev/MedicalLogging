37cdf0ad75fd72bc8a9ecfd568e72a07fc7a04c4
Kiefer Weis <kieferweis@protonmail.com>
2022-08-30 21:21:21 -0400

    installed dependencies, setup basic file structure

---
+++ middleware/auth.js
@@ -0,0 +1,11 @@
+module.exports = {
+  //middleware that sits between the request and the response and checks if the user is logged in
+    ensureAuth: function (req, res, next) {
+      if (req.isAuthenticated()) {
+        return next() //next() is what you have to run in each piece of middleware to have it continue to the next piece of middleware and not hang once it is complete
+      } else {
+        res.redirect('/') //if not logged in, redirect you back to main page that you can log in
+      }
+    }
+  }
+  
---
+++ routes/todos.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const todosController = require('../controllers/todos') 
+const { ensureAuth } = require('../middleware/auth')
+
+router.get('/', ensureAuth, todosController.getTodos) //ensureAuth is a function that checks to make sure you're logged in (using ensureAuth function in middleware/auth.js)
+
+router.post('/createTodo', todosController.createTodo)
+
+router.put('/markComplete', todosController.markComplete)
+
+router.put('/markIncomplete', todosController.markIncomplete)
+
+router.delete('/deleteTodo', todosController.deleteTodo)
+
+module.exports = router
